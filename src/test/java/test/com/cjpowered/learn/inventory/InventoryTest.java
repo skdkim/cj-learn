b80ae3b337b8ef1cbe9341a6c482602b57b562b6
Sung Kwang David Kim <sungkwangkim@gmail.com>
2017-05-08 02:08:58 -0700

    refactor change variable names and organize whitespace

--- src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
+++ src/test/java/test/com/cjpowered/learn/inventory/InventoryTest.java
@@ -33,7 +33,6 @@ public class InventoryTest {
 			public List<Item> stockItems(){
 				// TODO Auto-generate method stub
 				return Collections.emptyList();
-//				return super.stockItems();
 			}
 		};
     	
@@ -41,144 +40,144 @@ public class InventoryTest {
         final InventoryManager im = new AceInventoryManager(db);
 
         // when
-        final List<Order> actual = im.getOrders(today);
+        final List<Order> actualOrders = im.getOrders(today);
 
         // then
-        assertTrue(actual.isEmpty());
+        assertTrue(actualOrders.isEmpty());
 
     }
     
     @Test
     public void orderEnoughStock(){
-	    	// given
-    		int onHand = 10;
-    		int shouldHave = 16;
-    		
-    		Item item = new StockedItem(shouldHave);
-    		final InventoryDatabase db = new DatabaseTemplate() {
-    			@Override
-    			public int onHand(Item item){
-    				// TODO Auto-generate method stub
-    				return onHand;
-    			}
-    			
-    			@Override
-    			public List<Item> stockItems(){
-    				// TODO Auto-generate method stub
-    				return Collections.singletonList(item);
-    			}
-    		};
-    		
-    		final InventoryManager im = new AceInventoryManager(db);
-    		final LocalDate today = LocalDate.now();
-    	
-	    	// when
-	    	final List<Order> actual = im.getOrders(today);
-    		
-	    	// then
-	    assertEquals(1, actual.size());
-	    assertEquals(item, actual.get(0).item);
-	    assertEquals(shouldHave - onHand, actual.get(0).quantity);
+    	// given
+		int onHand = 10;
+		int shouldHave = 16;
+		
+		Item item = new StockedItem(shouldHave);
+		final InventoryDatabase db = new DatabaseTemplate() {
+			@Override
+			public int onHand(Item item){
+				// TODO Auto-generate method stub
+				return onHand;
+			}
+			
+			@Override
+			public List<Item> stockItems(){
+				// TODO Auto-generate method stub
+				return Collections.singletonList(item);
+			}
+		};
+		
+		final InventoryManager im = new AceInventoryManager(db);
+		final LocalDate today = LocalDate.now();
+	
+    	// when
+    	final List<Order> actualOrders = im.getOrders(today);
+		
+    	// then
+	    assertEquals(1, actualOrders.size());
+	    assertEquals(item, actualOrders.get(0).item);
+	    assertEquals(shouldHave - onHand, actualOrders.get(0).quantity);
     }
     
     @Test
     public void orderStockWithSurplus(){
-	    	// given
-    		int onHand = 16;
-    		int shouldHave = 10;
-    		
-    		Item item = new StockedItem(shouldHave);
-    		final InventoryDatabase db = new DatabaseTemplate() {
-    			@Override
-    			public int onHand(Item item){
-    				// TODO Auto-generate method stub
-    				return onHand;
-    			}
-    			
-    			@Override
-    			public List<Item> stockItems(){
-    				// TODO Auto-generate method stub
-    				return Collections.singletonList(item);
-    			}
-    		};
-    		
-    		final InventoryManager im = new AceInventoryManager(db);
-    		final LocalDate today = LocalDate.now();
-    	
-	    	// when
-	    	final List<Order> actualOrders = im.getOrders(today);
-    		
-	    	// then
+    	// given
+		int onHand = 16;
+		int shouldHave = 10;
+		
+		Item item = new StockedItem(shouldHave);
+		final InventoryDatabase db = new DatabaseTemplate() {
+			@Override
+			public int onHand(Item item){
+				// TODO Auto-generate method stub
+				return onHand;
+			}
+			
+			@Override
+			public List<Item> stockItems(){
+				// TODO Auto-generate method stub
+				return Collections.singletonList(item);
+			}
+		};
+		
+		final InventoryManager im = new AceInventoryManager(db);
+		final LocalDate today = LocalDate.now();
+	
+    	// when
+    	final List<Order> actualOrders = im.getOrders(today);
+		
+    	// then
 	    assertEquals(0, actualOrders.size());
 	    assertTrue(actualOrders.isEmpty());
     }
     
     @Test
     public void orderStockWithEqualInventory(){
-	    	// given
-    		int onHand = 10;
-    		int shouldHave = 10;
-    		
-    		Item item = new StockedItem(shouldHave);
-    		final InventoryDatabase db = new DatabaseTemplate() {
-    			@Override
-    			public int onHand(Item item){
-    				// TODO Auto-generate method stub
-    				return onHand;
-    			}
-    			
-    			@Override
-    			public List<Item> stockItems(){
-    				// TODO Auto-generate method stub
-    				return Collections.singletonList(item);
-    			}
-    		};
-    		
-    		final InventoryManager im = new AceInventoryManager(db);
-    		final LocalDate today = LocalDate.now();
-    	
-	    	// when
-	    	final List<Order> actualOrders = im.getOrders(today);
-    		
-	    	// then
-		    assertEquals(0, actualOrders.size());
-		    assertTrue(actualOrders.isEmpty());
+    	// given
+		int onHand = 10;
+		int shouldHave = 10;
+		
+		Item item = new StockedItem(shouldHave);
+		final InventoryDatabase db = new DatabaseTemplate() {
+			@Override
+			public int onHand(Item item){
+				// TODO Auto-generate method stub
+				return onHand;
+			}
+			
+			@Override
+			public List<Item> stockItems(){
+				// TODO Auto-generate method stub
+				return Collections.singletonList(item);
+			}
+		};
+		
+		final InventoryManager im = new AceInventoryManager(db);
+		final LocalDate today = LocalDate.now();
+	
+    	// when
+    	final List<Order> actualOrders = im.getOrders(today);
+		
+    	// then
+	    assertEquals(0, actualOrders.size());
+	    assertTrue(actualOrders.isEmpty());
     }
     
     @Test
     public void orderEnoughMultipleStock(){
-        	// given
-    		int onHandA = 10;
-    		int onHandB = 12;
-    		int shouldHaveA = 16;
-    		int shouldHaveB = 20;
-    		
-    		Item itemA = new StockedItem(shouldHaveA);
-    		Item itemB = new StockedItem(shouldHaveB);
-    		final InventoryDatabase db = new DatabaseTemplate() {
-    			@Override
-    			public int onHand(Item item){
-    				// TODO Auto-generate method stub
-    				return item == itemA ? onHandA : onHandB;
-    			}
-    			
-    			@Override
-    			public List<Item> stockItems(){
-    				// TODO Auto-generate method stub
-    				List<Item> items = new ArrayList<Item>();
-    				items.add(itemA);
-    				items.add(itemB);
-    				return items;
-    			}
-    		};
-    		
-    		final InventoryManager im = new AceInventoryManager(db);
-    		final LocalDate today = LocalDate.now();
-    	
-        	// when
-        	final List<Order> actualOrders = im.getOrders(today);
-    		
-        	// then
+    	// given
+		int onHandA = 10;
+		int onHandB = 12;
+		int shouldHaveA = 16;
+		int shouldHaveB = 20;
+		
+		Item itemA = new StockedItem(shouldHaveA);
+		Item itemB = new StockedItem(shouldHaveB);
+		final InventoryDatabase db = new DatabaseTemplate() {
+			@Override
+			public int onHand(Item item){
+				// TODO Auto-generate method stub
+				return item == itemA ? onHandA : onHandB;
+			}
+			
+			@Override
+			public List<Item> stockItems(){
+				// TODO Auto-generate method stub
+				List<Item> items = new ArrayList<Item>();
+				items.add(itemA);
+				items.add(itemB);
+				return items;
+			}
+		};
+		
+		final InventoryManager im = new AceInventoryManager(db);
+		final LocalDate today = LocalDate.now();
+	
+    	// when
+    	final List<Order> actualOrders = im.getOrders(today);
+		
+    	// then
         assertEquals(2, actualOrders.size());
         assertEquals(itemA, actualOrders.get(0).item);
         assertEquals(itemB, actualOrders.get(1).item);
@@ -188,38 +187,38 @@ public class InventoryTest {
     
     @Test
     public void orderEnoughMultipleStockWithOneSurplus(){
-        	// given
-    		int onHandA = 10;
-    		int onHandB = 12;
-    		int shouldHaveA = 8;
-    		int shouldHaveB = 20;
-    		
-    		Item itemA = new StockedItem(shouldHaveA);
-    		Item itemB = new StockedItem(shouldHaveB);
-    		final InventoryDatabase db = new DatabaseTemplate() {
-    			@Override
-    			public int onHand(Item item){
-    				// TODO Auto-generate method stub
-    				return item == itemA ? onHandA : onHandB;
-    			}
-    			
-    			@Override
-    			public List<Item> stockItems(){
-    				// TODO Auto-generate method stub
-    				List<Item> items = new ArrayList<Item>();
-    				items.add(itemA);
-    				items.add(itemB);
-    				return items;
-    			}
-    		};
-    		
-    		final InventoryManager im = new AceInventoryManager(db);
-    		final LocalDate today = LocalDate.now();
-    	
-        	// when
-        	final List<Order> actualOrders = im.getOrders(today);
-    		
-        	// then
+    	// given
+		int onHandA = 10;
+		int onHandB = 12;
+		int shouldHaveA = 8;
+		int shouldHaveB = 20;
+		
+		Item itemA = new StockedItem(shouldHaveA);
+		Item itemB = new StockedItem(shouldHaveB);
+		final InventoryDatabase db = new DatabaseTemplate() {
+			@Override
+			public int onHand(Item item){
+				// TODO Auto-generate method stub
+				return item == itemA ? onHandA : onHandB;
+			}
+			
+			@Override
+			public List<Item> stockItems(){
+				// TODO Auto-generate method stub
+				List<Item> items = new ArrayList<Item>();
+				items.add(itemA);
+				items.add(itemB);
+				return items;
+			}
+		};
+		
+		final InventoryManager im = new AceInventoryManager(db);
+		final LocalDate today = LocalDate.now();
+	
+    	// when
+    	final List<Order> actualOrders = im.getOrders(today);
+		
+    	// then
         assertEquals(1, actualOrders.size());
         assertEquals(itemB, actualOrders.get(0).item);
         assertEquals(shouldHaveB - onHandB, actualOrders.get(0).quantity);
@@ -227,39 +226,39 @@ public class InventoryTest {
     
     @Test(expected=IndexOutOfBoundsException.class)
     public void orderEnoughMultipleStockWithOneSurplusError(){
-        	// given
-    		int onHandA = 10;
-    		int onHandB = 12;
-    		int shouldHaveA = 8;
-    		int shouldHaveB = 20;
-    		
-    		Item itemA = new StockedItem(shouldHaveA);
-    		Item itemB = new StockedItem(shouldHaveB);
-    		final InventoryDatabase db = new DatabaseTemplate() {
-    			@Override
-    			public int onHand(Item item){
-    				// TODO Auto-generate method stub
-    				return item == itemA ? onHandA : onHandB;
-    			}
-    			
-    			@Override
-    			public List<Item> stockItems(){
-    				// TODO Auto-generate method stub
-    				List<Item> items = new ArrayList<Item>();
-    				items.add(itemA);
-    				items.add(itemB);
-    				return items;
-    			}
-    		};
-    		
-    		final InventoryManager im = new AceInventoryManager(db);
-    		final LocalDate today = LocalDate.now();
-    	
-        	// when
-        	final List<Order> actualOrders = im.getOrders(today);
-    		
-        	// then
-        	Item nonExistantItem = actualOrders.get(1).item;
+    	// given
+		int onHandA = 10;
+		int onHandB = 12;
+		int shouldHaveA = 8;
+		int shouldHaveB = 20;
+		
+		Item itemA = new StockedItem(shouldHaveA);
+		Item itemB = new StockedItem(shouldHaveB);
+		final InventoryDatabase db = new DatabaseTemplate() {
+			@Override
+			public int onHand(Item item){
+				// TODO Auto-generate method stub
+				return item == itemA ? onHandA : onHandB;
+			}
+			
+			@Override
+			public List<Item> stockItems(){
+				// TODO Auto-generate method stub
+				List<Item> items = new ArrayList<Item>();
+				items.add(itemA);
+				items.add(itemB);
+				return items;
+			}
+		};
+		
+		final InventoryManager im = new AceInventoryManager(db);
+		final LocalDate today = LocalDate.now();
+	
+    	// when
+    	final List<Order> actualOrders = im.getOrders(today);
+		
+    	// then
+    	Item nonExistantItem = actualOrders.get(1).item;
     }
 }
 
